56b9c1205346e3f2e08e051051097c9548c86488
Abhirup <47302857+Abhirup-Kumar@users.noreply.github.com>
2021-05-29 15:40:26 +0530

    Add files via upload

---
+++ src/com/foxmula/assignment1_1/Main.java
@@ -0,0 +1,22 @@
+package com.foxmula.assignment1_1;
+//
+////shift+enter
+////ctrl+j
+////alt+4
+////ctrl+ctrl
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Time time1 = new Time(2, 30, 45);
+        Time time2 = new Time(1, 40, 15);
+        time1.add(time1, time2);
+    }
+}
+
+
+
+
+
+
